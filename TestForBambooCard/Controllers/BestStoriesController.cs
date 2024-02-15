68c339db44e94af4b9ebb23ef95df6f7f5a1ff03
Asad Butt <asadbutt97@gmail.com>
2024-02-15 14:58:28 +0500

    Add project files.

---
+++ TestForBambooCard/Controllers/BestStoriesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+namespace TestForBambooCard.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BestStoriesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMemoryCache _cache;
+        public BestStoriesController(IHttpClientFactory httpClientFactory, IMemoryCache cache)
+        {
+            _httpClientFactory = httpClientFactory;
+            _cache = cache;
+        }
+
+        [HttpGet("{n}")]
+        public async Task<ActionResult<IEnumerable<Story>>> GetBestStories(int n)
+        {
+            var cacheKey = $"BestStories_{n}";
+
+            if (_cache.TryGetValue(cacheKey, out List<Story> cachedStories))
+            {
+                return Ok(cachedStories);
+            }
+
+            List<int> bestStoryIds = await GetBestStoryIds(3);
+            List<Story> bestStories = new List<Story>();
+           
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                foreach (int storyId in bestStoryIds.GetRange(0, n))
+                {
+                    var response = await client.GetAsync($"https://hacker-news.firebaseio.com/v0/item/{storyId}.json");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var storyJson = await response.Content.ReadAsStringAsync();
+                        var story = JsonConvert.DeserializeObject<Story>(storyJson);
+                        bestStories.Add(story);
+                    }
+                }
+            }
+            bestStories = bestStories.OrderByDescending(s => s.Score).ToList();
+
+            _cache.Set(cacheKey, bestStories, TimeSpan.FromMinutes(10)); 
+
+            return Ok(bestStories);
+        }
+
+        private async Task<List<int>> GetBestStoryIds(int mmaxAttempts)
+        {
+            List<int> bestStoryIds = new List<int>();
+            int currentRetry = 0;
+
+            while (currentRetry < mmaxAttempts)
+            {
+                try
+                {
+                    using (var client = _httpClientFactory.CreateClient())
+                    {
+                        var response = await client.GetAsync("https://hacker-news.firebaseio.com/v0/beststories.json");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var bestStoryIdsJson = await response.Content.ReadAsStringAsync();
+                            bestStoryIds = JsonConvert.DeserializeObject<List<int>>(bestStoryIdsJson);
+                            return bestStoryIds;
+                        }
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    //error logging
+                }
+
+
+                int delay = (int)Math.Pow(2, currentRetry) * 1000;
+                await Task.Delay(delay);
+
+                currentRetry++;
+            }
+
+
+            return bestStoryIds;
+        }
+
+    }
+
+    public class Story
+    {
+        public string By { get; set; }
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
+
---
+++ TestForBambooCard/Middlewares/RequestLimitMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+
+namespace TestForBambooCard.Middlewares
+{
+    public class RequestLimitMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly int _maxRequests;
+        private readonly TimeSpan _interval;
+        private readonly Queue<DateTime> _requestTimes;
+
+        public RequestLimitMiddleware(RequestDelegate next, int maxRequests, TimeSpan interval)
+        {
+            _next = next;
+            _maxRequests = maxRequests;
+            _interval = interval;
+            _semaphore = new SemaphoreSlim(maxRequests);
+            _requestTimes = new Queue<DateTime>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _semaphore.WaitAsync();
+
+            try
+            {
+                EnforceRateLimit();
+
+                await _next(context);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private void EnforceRateLimit()
+        {
+            var now = DateTime.Now;
+
+            // Remove expired request times
+            while (_requestTimes.Count > 0 && now - _requestTimes.Peek() > _interval)
+            {
+                _requestTimes.Dequeue();
+            }
+
+            // Add current request time
+            _requestTimes.Enqueue(now);
+
+            // Check if the number of requests exceeds the limit
+            if (_requestTimes.Count > _maxRequests)
+            {
+                throw new RequestLimitExceededException("Rate limit exceeded. Please try again later.");
+            }
+        }
+
+        public class RequestLimitExceededException : Exception
+        {
+            public RequestLimitExceededException(string? message) : base(message)
+            {
+            }
+        }
+    }
+}
